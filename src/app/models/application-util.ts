8966b78c9a0cca8d2e0b3ff8da6218a43206374d
Carlos <carlos.ramirez1@unmsm.edu.pe>
2017-08-29 13:09:53 -0500

    docker

--- src/app/models/application-util.ts
+++ src/app/models/application-util.ts
@@ -1,8 +1,19 @@
 import { GeoPoint } from './geoPoint';
+import { Location } from '@angular/common';
+// Get the hostname
+this.hostname = location.host;
+if (this.hostname.indexOf(':') > 0) {
+ this.hostname = this.hostname.substr(0, this.hostname.indexOf(':'));
+}
+// Add a port or a subdomain to get the API url:
+
+this.apiUrl = 'http://' + this.hostname + ':8080';
+const urlBase = this.hostname;
+
 export class AppUtil {
-  public static HTTP: string = 'https://';
-  public static HTTPS: string = 'https://';
-  public static IP: string = 'cocheras-lb.herokuapp.com';
+  public static HTTP: string = 'http://';
+  public static HTTPS: string = 'http://';
+  public static IP: string = urlBase+':3000';
   /*public static IP: string = 'localhost';*/
   public static PORT: string = '3000';
   public static TIPO_SERVICIO_API: string = 'api/tipoServicios';
